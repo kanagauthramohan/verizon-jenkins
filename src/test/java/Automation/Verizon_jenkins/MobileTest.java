0e14eb0d02efe744c3d284afb991adfa9215cc2b
kanagauthra <uthrakanaga@gmail.com>
2023-07-26 14:12:08 +0530

    initial commit

---
+++ src/test/java/Automation/Verizon_jenkins/MobileTest.java
@@ -0,0 +1,23 @@
+package Automation.Verizon_jenkins;
+
+import org.testng.annotations.Test;
+
+public class MobileTest 
+{
+   
+    @Test
+    
+    void login()
+    {
+    System.out.println("Login");
+    }
+    
+    @Test
+   
+    void Logut()
+    {
+     System.out.println("Logout");
+  
+    }
+    
+}
